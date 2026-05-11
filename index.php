05c489af064182a95b0185db7114cb18849d5c52
aner <monzon.aner@gmail.com>
2026-05-11 20:52:04 +0200

    Estructura inicial index.php y su css para emezar a trabajar

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>isunKi - Gestión de Multas</title>
+    <link rel="stylesheet" href="css/estilos.css">
+</head>
+<body>
+    <div class="contenedorPrincipal">
+        <header class="cabezera">
+            <div class="logo-container">
+                <div class="logo-circular"><img src="img/logoPequeño.png" width="56" height="50"></img></div>
+                <div class="logo-escrito"><img src="img/logoTexto.png" width="130" height="40"></div>
+            </div>
+            
+            <nav class="navegador">
+                <ul>
+                    <li><a href="index.php?page=ranking">Ranking</a></li>
+                    <li><a href="index.php?page=mis-multas">Mis Multas</a></li>
+                    <li><a href="index.php?page=login" class="btn-login">Entrar</a></li>
+                </ul>
+            </nav>
+        </header>
+
+        <main class="content">
+            <?php
+            ?>
+        </main>
+
+        <footer class="main-footer">
+            <p>&copy; 2026 isunKi - Proyecto Final de Grado</p>
+            <p>Diseñado para la gestión deportiva eficiente.</p>
+        </footer>
+
+    </div>
+
+</body>
+</html>
